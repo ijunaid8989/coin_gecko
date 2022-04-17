e6b18ccfc0814f952f6652cc63ffd556c161325a
Junaid Farooq <junaid@evercam.io>
2022-04-17 13:29:29 +0500

    Initial Commit.

---
+++ config/test.exs
@@ -0,0 +1,14 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :coin_gecko, CoinGeckoWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "b1OHkr7cDKTL4VqGhsmFjdOAUSbCgEonWyAsDysrhFkd608YT7Csof9SurIpquS+",
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
