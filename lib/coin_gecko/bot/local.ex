3e5449611671b2c04f7ba9537943935269c7677e
Junaid Farooq <junaid@evercam.io>
2022-04-18 02:11:07 +0500

    Set Mark seen.

--- lib/coin_gecko/bot/local.ex
+++ lib/coin_gecko/bot/local.ex
@@ -2,4 +2,8 @@ defmodule CoinGecko.Bot.Local do
   def set_get_started_with_greetings() do
     {:ok, %{"result" => "success"}}
   end
+
+  def set_mark_seen(recipient_id) do
+    {:ok, %{"recipient_id" => recipient_id}}
+  end
 end
--- lib/coin_gecko/bot/messaging.ex
+++ lib/coin_gecko/bot/messaging.ex
@@ -1,6 +1,6 @@
 defmodule CoinGecko.Bot.Messaging do
   def set_get_started_with_greetings() do
-    body =
+    message =
       Jason.encode!(%{
         "get_started" => %{
           "payload" => "GET_STARTED_PAYLOAD"
@@ -9,19 +9,42 @@ defmodule CoinGecko.Bot.Messaging do
           %{
             "locale" => "default",
             "text" => "Hello {{user_first_name}}! how are you doing today?"
-          },
+          }
         ]
       })
 
-    headers = [{"Content-type", "application/json"}]
+    messenger_profile_api()
+    |> post(message)
+  end
 
-    (messenger_profile_api() <> "?access_token=" <> access_token())
-    |> HTTPoison.post(body, headers, [])
-    |> IO.inspect()
+  def set_mark_seen(recipient_id) do
+    message =
+      Jason.encode!(%{
+        "recipient" => %{
+          "id" => recipient_id
+        },
+        "sender_action" => "mark_seen"
+      })
+
+    messages_api()
+    |> post(message)
   end
 
   defp messenger_profile_api(), do: Application.get_env(:coin_gecko, :messenger_profile_api)
-  defp messages_api(), do: Application.get_env(:coin_gecko, :messages)
+  defp messages_api(), do: Application.get_env(:coin_gecko, :messages_api)
   defp access_token(), do: Application.get_env(:coin_gecko, :webhook_token)
   defp graph_api(), do: Application.get_env(:coin_gecko, :graph)
+
+  defp process_response_body({:ok, %HTTPoison.Response{body: body, status_code: 200}}),
+    do: Jason.decode(body)
+
+  defp process_response_body(_response), do: {:ok, ""}
+
+  defp post(url, message) do
+    headers = [{"Content-type", "application/json"}]
+
+    (url <> "?access_token=" <> access_token())
+    |> HTTPoison.post(message, headers, [])
+    |> process_response_body()
+  end
 end
